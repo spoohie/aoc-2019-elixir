758f2d3c61e38ad782683b75b9c456315aaeaef0
spoohie <kplatkowski@outlook.com>
2020-03-15 22:41:34 +0100

    Add function for running both day solutions

--- lib/helpers/run.ex
+++ lib/helpers/run.ex
@@ -1,7 +1,14 @@
-defmodule Mix.Tasks.Aoc.Run do
+defmodule Mix.Tasks.Aoc do
     use Mix.Task
 
-    @shortdoc "Run given 'day', 'part' application"
+    @shortdoc "Run given 'day' solutions"
+    def run([day]) do
+        module = "Elixir.Aoc.Day" <> day <> "_"
+        apply(String.to_existing_atom(module <> "1"), :run, [])
+        apply(String.to_existing_atom(module <> "2"), :run, [])
+    end
+
+    @shortdoc "Run given 'day' 'part' solution"
     def run([day, part]) do
         module = "Elixir.Aoc.Day" <> day <> "_" <> part
         apply(String.to_existing_atom(module), :run, [])
